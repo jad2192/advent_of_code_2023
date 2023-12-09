02b3bb631ab2ca230f5454d5da43cacd92249ad4
James Diotte <JamesDiotte@gmail.com>
2023-12-09 00:28:34 -0500

    day 9

---
+++ solutions/day09.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+def load_sequences(input_fp: str) -> List[List[int]]:
+    return [[int(d) for d in line.split()] for line in open(input_fp).read().split("\n")]
+
+
+def extrapolate_sequence(sequence: List[int], extrap_forward: bool) -> int:
+    differences = [sequence]
+    cur_seq = sequence
+    while set(cur_seq) != {0} and len(cur_seq) > 1:
+        cur_seq = [cur_seq[k + 1] - cur_seq[k] for k in range(len(cur_seq) - 1)]
+        differences.append(cur_seq)
+    exp_val = 0
+    for diff_seq in differences[::-1]:
+        if extrap_forward:
+            exp_val += diff_seq[-1]
+        else:
+            exp_val = diff_seq[0] - exp_val
+    return exp_val
+
+
+def sum_extrapolated_vals(input_fp: str, extrap_forward: bool = True) -> int:
+    sequences = load_sequences(input_fp)
+    return sum([extrapolate_sequence(seq, extrap_forward) for seq in sequences])
+
+
+# Tests
+assert sum_extrapolated_vals("inputs/day09/test.txt") == 114
+assert sum_extrapolated_vals("inputs/day09/test.txt", False) == 2
+
+
+# Solutions
+print(f"Part 1: Sum of Extrapolated Vals - {sum_extrapolated_vals('inputs/day09/main.txt')}")
+print(f"Part 2: Sum of Extrapolated Vals - {sum_extrapolated_vals('inputs/day09/main.txt', False)}")
